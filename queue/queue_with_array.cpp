246932ce402cdc96a49fbf0a5412525423df3950
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2024-08-26 00:26:54 +0600

    queue

--- queue/queue_with_array.cpp
+++ queue/queue_with_array.cpp
@@ -10,13 +10,62 @@ class Queue{
     int rear;
     int *arr;
     Queue(int size){
+        cout<<"Created queue with: "<<size<<endl;
         this->size = size;
         arr = new int[size];
         front = 0;
         rear = 0;
     }
 
-    // push 
-    void push()
+    // _____ push _________
+    void push(int data){
+        if(rear==size){
+            cout<<"queue is full"<<endl;
+            return ;
+        }
+      arr[rear++] = data;  
+    }
 
+    // _____ pop _________
+    void pop(){
+        if(front==rear){
+            cout<<"queue is empty"<<endl;
+            return ;
+        }
+        arr[front] = -1;
+        front++;
+        if(front==rear){
+            front=0;
+            rear=0;
+        }
+    }
+    // _________ front _____________
+
+    void top(){
+        if(front==rear){
+            cout<<"Queue is empty"<<endl;
+            return ;
+        }
+      cout<<arr[front]<<endl;
+    }
+    // _________ is_empty _____________
+
+    void isEmpty(){
+        if(front==rear){
+        cout<<"Queue is empty."<<endl;
+    }else{
+        cout<<"Queue is not empty"<<endl;
+    }
+    }
+    
 };
+
+int main(){
+    Queue q(5);
+    q.push(5);
+    q.push(5);
+    q.push(5);
+    q.push(5);
+    q.push(5);
+    q.isEmpty();
+}
